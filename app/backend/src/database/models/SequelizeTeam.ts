c29dc95996a80592fb55039e8967ffc4cbfc9358
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-09-04 00:19:00 -0300

    build: creates teams migration and sequelize model

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,35 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'trybeEval',
+  timestamps: false,
+});
+
+export default SequelizeTeam;
